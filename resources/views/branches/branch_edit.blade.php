8bf9743c70ea9930183fcd12a09b7a052083319d
Sayco-ahmed <59507720+Sayco-ahmed@users.noreply.github.com>
2020-12-09 02:37:11 +0200

    update

--- resources/views/branches/branch_edit.blade.php
+++ resources/views/branches/branch_edit.blade.php
@@ -76,9 +76,9 @@
                             <div class="date-picker-inner" style="text-align:left">
                                 
                                 <div class="form-group data-custon-pick" style="text-align:left">
-                                    <label>Branch Id</label>
+                                    <label>Branch Code</label>
                                     <div class="input-mark-inner mg-b-22">
-                                        <input type="number" name="branchCode" class="form-control" placeholder="Branch Id" value="{{$requested_branch->branchCode}}">
+                                        <input type="number" name="branchCode" class="form-control" placeholder="Branch Code" value="{{$requested_branch->branchCode}}">
                                     </div>
                                 </div>
                                 <div class="form-group data-custon-pick" style="text-align:left">
